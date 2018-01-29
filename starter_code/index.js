82c41db5a12d84adc860f2e60b36d552dcc5f86a
cut-throat <rober.strada@outlook.com>
2018-01-29 22:57:12 +0100

    done

--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,15 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+let e = new Elevator();
+
+e.start();
+e.call(new Person("Jesus",3,2));
+e.call(new Person("Alvaro",10,3));
+e.call(new Person("Miguel",4,9));
+e.call(new Person("Sergio",6,2));
+e.call(new Person("Nacho",1,8));
+e.call(new Person("Ana",5,10));
+
+
+
